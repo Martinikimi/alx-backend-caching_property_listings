f586aef7088018d75c5393fb25afbad2f3b0566f
Martin Kimani <martinikimi7@gmail.com>
2025-11-08 01:39:59 +0300

    Update utils.py

--- properties/utils.py
+++ properties/utils.py
@@ -7,3 +7,14 @@ def get_all_properties():
         properties = Property.objects.all()
         cache.set('all_properties', properties, 3600)
     return properties
+
+def get_redis_cache_metrics():
+    keyspace_hits = cache._cache.get_client().info().get('keyspace_hits', 0)
+    keyspace_misses = cache._cache.get_client().info().get('keyspace_misses', 0)
+    total_requests = keyspace_hits + keyspace_misses
+    hit_ratio = keyspace_hits / total_requests if total_requests > 0 else 0
+    return {
+        'keyspace_hits': keyspace_hits,
+        'keyspace_misses': keyspace_misses,
+        'hit_ratio': hit_ratio
+    }
